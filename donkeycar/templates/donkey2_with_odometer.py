0f704f55e11e6b7e130fc280a60f1a2127de6d66
Alan Wells <alan@adub.net>
2017-09-10 15:59:02 -0700

    update odometer template to match latest default template

--- donkeycar/templates/donkey2_with_odometer.py
+++ donkeycar/templates/donkey2_with_odometer.py
@@ -1,46 +1,150 @@
-# -*- coding: utf-8 -*-
-
+#!/usr/bin/env python3
 """
-Script to drive a donkey car using a webserver hosted on the vehicle.
+Scripts to drive a donkey 2 car and train a model for it. 
 
+Usage:
+    car.py (drive) [--model=<model>]
+    car.py (train) (--tub=<tub>) (--model=<model>)
+    car.py (calibrate) 
 """
+
+import os
+from docopt import docopt
 import donkeycar as dk 
 
-V = dk.vehicle.Vehicle()
-
-cam = dk.parts.PiCamera()
-V.add(cam, outputs=['cam/image_array'], threaded=True)
-
-ctr = dk.parts.LocalWebController()
-V.add(ctr, 
-      inputs=['cam/image_array'],
-      outputs=['user/angle', 'user/throttle', 'user/mode'],
-      threaded=True)
+CAR_PATH = PACKAGE_PATH = os.path.dirname(os.path.realpath(__file__))
+DATA_PATH = os.path.join(CAR_PATH, 'data')
+MODELS_PATH = os.path.join(CAR_PATH, 'models')
 
 
-steering_controller = dk.parts.PCA9685(1)
-steering = dk.parts.PWMSteering(controller=steering_controller,
-                                left_pulse=460, right_pulse=260)
+def drive(model_path=None):
+    #Initialized car
+    V = dk.vehicle.Vehicle()
+    cam = dk.parts.PiCamera()
+    V.add(cam, outputs=['cam/image_array'], threaded=True)
+    
+    ctr = dk.parts.LocalWebController()
+    V.add(ctr, 
+          inputs=['cam/image_array'],
+          outputs=['user/angle', 'user/throttle', 'user/mode', 'recording'],
+          threaded=True)
+    
+    #See if we should even run the pilot module. 
+    #This is only needed because the part run_contion only accepts boolean
+    def pilot_condition(mode):
+        if mode == 'user':
+            return False
+        else:
+            return True
+        
+    pilot_condition_part = dk.parts.Lambda(pilot_condition)
+    V.add(pilot_condition_part, inputs=['user/mode'], outputs=['run_pilot'])
+    
+    #Run the pilot if the mode is not user.
+    kl = dk.parts.KerasCategorical()
+    if model_path:
+        kl.load(model_path)
+    
+    V.add(kl, inputs=['cam/image_array'], 
+          outputs=['pilot/angle', 'pilot/throttle'],
+          run_condition='run_pilot')
+    
+    
+    #Choose what inputs should change the car.
+    def drive_mode(mode, 
+                   user_angle, user_throttle,
+                   pilot_angle, pilot_throttle):
+        if mode == 'user': 
+            return user_angle, user_throttle
+        
+        elif mode == 'local_angle':
+            return pilot_angle, user_throttle
+        
+        else: 
+            return pilot_angle, pilot_throttle
+        
+    drive_mode_part = dk.parts.Lambda(drive_mode)
+    V.add(drive_mode_part, 
+          inputs=['user/mode', 'user/angle', 'user/throttle',
+                  'pilot/angle', 'pilot/throttle'], 
+          outputs=['angle', 'throttle'])
+    
+    
+    steering_controller = dk.parts.PCA9685(1)
+    steering = dk.parts.PWMSteering(controller=steering_controller,
+                                    left_pulse=523, right_pulse=255)
+    
+    throttle_controller = dk.parts.PCA9685(0)
+    throttle = dk.parts.PWMThrottle(controller=throttle_controller,
+                                    max_pulse=500, zero_pulse=370, min_pulse=220)
+    
+    V.add(steering, inputs=['angle'])
+    V.add(throttle, inputs=['throttle'])
 
-throttle_controller = dk.parts.PCA9685(0)
-throttle = dk.parts.PWMThrottle(controller=throttle_controller,
-                                max_pulse=500, zero_pulse=370, min_pulse=220)
+    odometer = dk.parts.RotaryEncoder(m_per_tick=0.0329, pin=23)
+    V.add(odometer, outputs=['odometer/meters', 'odometer/meters_per_second'], threaded=True)
+    
+    #add tub to save data
+    inputs=['cam/image_array',
+            'user/angle', 'user/throttle', 
+            #'pilot/angle', 'pilot/throttle', 
+            'user/mode', 'odometer/meters', 'odometer/meters_per_second']
+    types=['image_array',
+           'float', 'float',  
+           #'float', 'float', 
+           'str', 'float', 'float']
+    
+    th = dk.parts.TubHandler(path=DATA_PATH)
+    tub = th.new_tub_writer(inputs=inputs, types=types)
+    V.add(tub, inputs=inputs, run_condition='recording')
+    
+    #run the vehicle for 20 seconds
+    V.start(rate_hz=20, max_loop_count=100000)
+    
+    print("You can now go to <your pi ip address>:8887 to drive your car.")
 
-V.add(steering, inputs=['user/angle'])
-V.add(throttle, inputs=['user/throttle'])
 
-odometer = dk.parts.RotaryEncoder(m_per_tick=0.0329, pin=23)
-V.add(odometer, outputs=['odometer/meters', 'odometer/meters_per_second'], threaded=True)
 
-#add tub to save data
-path='~/mydonkey/sessions/odometer_with_dist'
-inputs=['user/angle', 'user/throttle', 'cam/image_array', 
-        'odometer/meters', 'odometer/meters_per_second']
-types=['float', 'float', 'image_array', 'float', 'float']
-tub=dk.parts.TubWriter(path, inputs=inputs, types=types)
-V.add(tub, inputs=inputs)
+def train(tub_name, model_name):
+    
+    kl = dk.parts.KerasCategorical()
+    
+    tub_path = os.path.join(DATA_PATH, tub_name)
+    tub = dk.parts.Tub(tub_path)
+    
+    X_keys = ['cam/image_array']
+    y_keys = ['user/angle', 'user/throttle']
+    
+    def rt(record):
+        record['user/angle'] = dk.utils.linear_bin(record['user/angle'])
+        return record
+    
+    train_gen, val_gen = tub.train_val_gen(X_keys, y_keys, 
+                                           record_transform=rt, batch_size=128)
+    
+    model_path = os.path.join(MODELS_PATH, model_name)
+    kl.train(train_gen, val_gen, saved_model_path=model_path)
 
-#run the vehicle for 20 seconds
-V.start(rate_hz=10, max_loop_count=1000)
 
-#you can now go to localhost:8887 to move a square around the image
+
+
+def calibrate():
+    channel = int(input('Enter the channel your actuator uses (0-15).'))
+    c = dk.parts.PCA9685(channel)
+    
+    for i in range(10):
+        pmw = int(input('Enter a PWM setting to test(100-600)'))
+        c.run(pmw)
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+
+    if args['drive']:
+        drive(model_path = args['--model'])
+    elif args['calibrate']:
+        calibrate()
+    elif args['train']:
+        tub = args['--tub']
+        model = args['--model']
+        train(tub, model)
