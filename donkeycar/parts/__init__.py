72e8669147dd018da046c34a70f4ae0cba16fae5
Alan Wells <alan@adub.net>
2017-10-19 20:32:14 -0700

    add customized categorical model and cropping

--- donkeycar/parts/__init__.py
+++ donkeycar/parts/__init__.py
@@ -28,6 +28,7 @@ from .sensors.teensy_rcin import TeensyRCin
 
 from .ml.keras import KerasCategorical
 from .ml.keras import KerasLinear
+from .ml.keras import AlanCategorical
 
 from .stores.original import OriginalWriter
 
--- donkeycar/parts/ml/keras.py
+++ donkeycar/parts/ml/keras.py
@@ -20,6 +20,7 @@ from ... import utils
 
 import donkeycar as dk
 from donkeycar import utils
+import pdb
 
 class KerasPilot():
  
@@ -45,7 +46,7 @@ class KerasPilot():
         #stop training if the validation error stops improving.
         early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', 
                                                    min_delta=.0005, 
-                                                   patience=10,
+                                                   patience=5,
                                                    verbose=1, 
                                                    mode='auto')
         
@@ -76,6 +77,23 @@ class KerasCategorical(KerasPilot):
         #angle_certainty = max(angle_binned[0])
         angle_unbinned = utils.linear_unbin(angle_binned)
         return angle_unbinned, throttle[0][0]
+
+class AlanCategorical(KerasPilot):
+    def __init__(self, model=None, *args, **kwargs):
+        super(AlanCategorical, self).__init__(*args, **kwargs)
+        if model:
+            self.model = model
+        else:
+            self.model = alan_categorical()
+        
+    def run(self, img_arr):
+        img_arr = image[45:,:]
+        img_arr = img_arr.reshape((1,) + img_arr.shape)
+        angle_binned, throttle_binned = self.model.predict(img_arr)
+
+        angle_unbinned = utils.linear_unbin(angle_binned)
+        throttle_unbinned = utils.linear_unbin(throttle_binned)
+        return angle_unbinned, throttle_unbinned
     
     
     
@@ -91,8 +109,6 @@ class KerasLinear(KerasPilot):
         angle, throttle = self.model.predict(img_arr)
         #angle_certainty = max(angle_binned[0])
         return angle[0][0], throttle[0][0]
-
-
 
 
 
@@ -242,5 +258,41 @@ def nvidia_categorical():
                   loss={'angle_out': 'categorical_crossentropy', 
                         'throttle_out': 'mean_absolute_error'},
                   loss_weights={'angle_out': 0.5, 'throttle_out': 0.5})
+    
+    return model
+
+def alan_categorical():
+    from keras.layers import Input, Dense, merge
+    from keras.models import Model
+    from keras.layers import Convolution2D, MaxPooling2D, Reshape, BatchNormalization, Cropping2D
+    from keras.layers import Activation, Dropout, Flatten, Dense
+    
+    img_in = Input(shape=(75, 160, 3), name='img_in')  
+    x = img_in
+    x = Cropping2D(cropping=((25,0), (0,0)))(x)
+    x = BatchNormalization(epsilon=0.001, axis=1)(x)
+    x = Convolution2D(24, (5,5), strides=(2,2), padding="same", activation='relu')(x)       # 24 features, 5 pixel x 5 pixel kernel (convolution, feauture) window, 2wx2h stride, relu activation
+    x = Convolution2D(32, (5,5), strides=(2,2), padding="same", activation='relu')(x)       # 32 features, 5px5p kernel window, 2wx2h stride, relu activatiion
+    x = Convolution2D(64, (5,5), strides=(2,2), padding="same", activation='relu')(x)       # 64 features, 5px5p kernal window, 2wx2h stride, relu
+    x = Convolution2D(64, (3,3), strides=(2,2), padding="same", activation='relu')(x)       # 64 features, 3px3p kernal window, 2wx2h stride, relu
+    x = Convolution2D(64, (3,3), strides=(1,1), padding="same", activation='relu')(x)       # 64 features, 3px3p kernal window, 1wx1h stride, relu
+
+    # Possibly add MaxPooling (will make it less sensitive to position in image).  Camera angle fixed, so may not to be needed
+
+    x = Flatten(name='flattened')(x)                                        # Flatten to 1D (Fully connected)
+    x = Dense(100, activation='relu')(x)                                    # Classify the data into 100 features, make all negatives 0
+    x = Dropout(.25)(x)                                                      # Randomly drop out (turn off) 10% of the neurons (Prevent overfitting)
+    x = Dense(50, activation='relu')(x)                                     # Classify the data into 50 features, make all negatives 0
+    x = Dropout(.25)(x)                                                      # Randomly drop out 10% of the neurons (Prevent overfitting)
+    
+    #categorical output of the angle
+    angle_out = Dense(15, activation='softmax', name='angle_out')(x)        # Connect every input with every output and output 15 hidden units. Use Softmax to give percentage. 15 categories and find best one based off percentage 0.0-1.0
+    throttle_out = Dense(15, activation='softmax', name='throttle_out')(x) 
+    
+    model = Model(inputs=[img_in], outputs=[angle_out, throttle_out])
+    model.compile(optimizer='adam',
+                  loss={'angle_out': 'categorical_crossentropy', 
+                        'throttle_out': 'categorical_crossentropy'},
+                  loss_weights={'angle_out': 0.5, 'throttle_out': .5})
     
     return model
